9a01e45a13e32e5e9f76bcfeffe1360615e0eeeb
Aditya Galang Adzana <aditya.galang.adzana@polytron.co.id>
2025-07-16 11:20:57 +0700

    progress csrf

--- pages/csrf/lab-2/index.php
+++ pages/csrf/lab-2/index.php
@@ -5,8 +5,6 @@ require_once '../../../template/header.php';
 
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
 $message = '';
 
